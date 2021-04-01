3414ae42ac5bcd656639f2662908f09463a76eb2
Jeff Jadulco <dev@jeffjadulco.com>
2021-04-02 00:35:49 +0800

    :sparkles: Add placeholder for empty bookmarks

--- src/components/bookmarks.js
+++ src/components/bookmarks.js
@@ -1,5 +1,7 @@
+import Link from "next/link"
 import { useEffect } from "react"
 import { useBookmarks } from "../hooks/useBookmarks"
+import { IconBookmarkOutline } from "./icons"
 import PostCard from "./postCard"
 
 const Bookmarks = () => {
@@ -11,9 +13,25 @@ const Bookmarks = () => {
 
   return (
     <main className="flex-1">
-      {bookmarksData &&
-        bookmarksData.map((data) => <PostCard key={data.id} data={data} />)}
+      {bookmarksData && bookmarksData.length > 0 ? (
+        bookmarksData.map((data) => <PostCard key={data.id} data={data} />)
+      ) : (
+        <NoBookmarks />
+      )}
     </main>
+  )
+}
+
+const NoBookmarks = () => {
+  return (
+    <div>
+      <h3 className="text-2xl text-gray-600 font-bold text-center sm:text-left">
+        No bookmarks 😞
+      </h3>
+      <p className=" text-gray-500 text-center sm:text-left">
+        Press the save icon <IconBookmarkOutline /> on any post to bookmark it.
+      </p>
+    </div>
   )
 }
 
